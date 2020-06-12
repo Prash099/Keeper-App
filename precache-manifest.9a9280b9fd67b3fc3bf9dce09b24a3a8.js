7268e17f79698d4fe3b1dafe631d8494ef8815b2
prashanthr-099 <63853479+prashanthr-099@users.noreply.github.com>
2020-06-12 19:43:41 +0530

    Updates

---
+++ precache-manifest.9a9280b9fd67b3fc3bf9dce09b24a3a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d004f714a63e9ea66b3684918731097",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e25b95467c191baddb65",
+    "url": "/Keeper-App/static/js/2.4cc7a95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Keeper-App/static/js/2.4cc7a95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcd62f3efd47fc795f8",
+    "url": "/Keeper-App/static/js/main.50b1dea1.chunk.js"
+  },
+  {
+    "revision": "4da9893e7fb7c1cd91da",
+    "url": "/Keeper-App/static/js/runtime-main.a46e3a74.js"
+  }
+]);
